8e1d706089de98d2d038a3e2cb28713d77b77674
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-11-03 07:45:41 +0530

    remove firebase config

--- src/components/Addtophoto.jsx
+++ src/components/Addtophoto.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { getFirestore, collection, addDoc } from 'firebase/firestore/lite';
-import { initializeApp } from 'firebase/app';
+
 import Admintop from './Admintop';
 import Navbar from './Navbar';
 import "./admin.css"
@@ -8,18 +7,6 @@ import "./admin.css"
 //import Sidebar from './Sidebar';
 
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDdyTqvViyqtLETSrNMzqFP-xM0NIa9lQ8",
-    authDomain: "clashlayouts-9d19d.firebaseapp.com",
-    projectId: "clashlayouts-9d19d",
-    storageBucket: "clashlayouts-9d19d.appspot.com",
-    messagingSenderId: "1073716509146",
-    appId: "1:1073716509146:web:56713b2e4bf310187b28e3"
-  };
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 const Addtopphoto = () => {
   const [description, setDescription] = useState('');
